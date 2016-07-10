408eea55e13c94d08c1524b2c8bc07116f7450cd
koziscool <kozjob@yahoo.com>
2016-07-09 19:11:29 -0500

    stuff

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var controller = {
+
+
+
+};
+
+
+$(document).ready( function() {
+
+  view.init();
+
+});
---
+++ model.js
@@ -0,0 +1,11 @@
+
+var space = {
+
+  asteroids: [],
+
+  MAX_VELOCITY: 1.8,
+  width: 1200,
+  height: 900,
+
+  minAsteroidSize: 20
+};
